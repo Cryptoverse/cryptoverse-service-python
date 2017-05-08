a584b1b3e316a0b0cdc70bcddd5d4de41e36d4f8
Brian Ostrander <Ostrander.Brian@gmail.com>
2017-05-07 23:20:31 -0400

    Added jump cost checking to event posting.

--- app.py
+++ app.py
@@ -438,12 +438,14 @@ def postEvents():
 		eventSignature = EventSignature(util.getEventTypeId(eventJson['type']), fleet.id, eventJson['hash'], eventJson['signature'], util.getTime(), 0)
 		session.add(eventSignature)
 		session.flush()
-
+		inputs = []
 		for currentInput in eventJson['inputs']:
 			targetInput = session.query(Event).filter_by(key=currentInput['key']).first()
 			if targetInput is None:
 				raise Exception('event with key %s not accounted for' % currentInput['key'])
+			inputs.append(targetInput)
 			session.add(EventInput(targetInput.id, eventSignature.id, currentInput['index']))
+		outputs = []
 		for currentOutput in eventJson['outputs']:
 			targetOutput = session.query(Event).filter_by(key=currentOutput['key']).first()
 			if targetOutput is None:
@@ -459,8 +461,49 @@ def postEvents():
 				targetOutput = Event(currentOutput['key'], util.getEventTypeId(currentOutput['type']), outputFleet.id, currentOutput['count'], targetStarSystem.id)
 				session.add(targetOutput)
 				session.flush()
+			outputs.append(targetOutput)
 			eventOutput = EventOutput(targetOutput.id, eventSignature.id, currentOutput['index'])
 			session.add(eventOutput)
+		if eventJson['type'] == 'jump':
+			if len(inputs) == 0:
+				raise Exception('jump must contain at least one input')
+			if len(outputs) == 0:
+				raise Exception('jump must contain at least oun output')
+			inputShipCount = 0
+			originSystemId = inputs[0].star_system_id
+			for currentInput in inputs:
+				if currentInput.fleet_id != fleet.id:
+					raise Exception('jump must consist of ships from a single fleet')
+				if currentInput.star_system_id != originSystemId:
+					raise Exception('jump inputs must start from the same origin')
+				inputShipCount += currentInput.count
+			outputShipCount = 0
+			destinationSystemId = None
+			for currentOutput in outputs:
+				if currentOutput.star_system_id != originSystemId:
+					destinationSystemId = currentOutput.star_system_id
+					break
+			if destinationSystemId is None:
+				raise Exception('jump must consist of at least one output in another system')
+			for currentOutput in outputs:
+				if currentOutput.fleet_id != fleet.id:
+					raise Exception('jump must consist of ships from a single fleet')
+				if currentOutput.star_system_id == originSystemId:
+					inputShipCount -= currentOutput.count
+				elif currentOutput.star_system_id == destinationSystemId:
+					outputShipCount += currentOutput.count
+				else:
+					raise Exception('jump outputs must end in the same origin or destination')
+			originSystem = session.query(StarLog).filter_by(id=originSystemId).first()
+			destinationSystem = session.query(StarLog).filter_by(id=destinationSystemId).first()
+			shipCost = util.getJumpCost(originSystem.hash, destinationSystem.hash, inputShipCount)
+			if shipCost == inputShipCount:
+				raise Exception('jump cannot have zero ships reach destination')
+			if shipCost != (inputShipCount - outputShipCount):
+				raise Exception('jump cost does not match expected cost of %s' % shipCost)
+		else:
+			raise Exception('event type %s not supported' % eventJson['type'])
+
 		session.commit()
 		return '200', 200
 	except:
