27c26cb9ae5e7bf3af903d42fbc986380ad179ce
Brian Ostrander <Ostrander.Brian@gmail.com>
2017-06-04 08:19:24 -0400

    Updated run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Make sure environmental variables are set before running this script.
 if [ $NO_GATEWAY == '1' ]; then
-	python app.py
+	python project/app.py
 else
 	`which gunicorn` -w 4 -b 127.0.0.1:5000 app:app
 fi
